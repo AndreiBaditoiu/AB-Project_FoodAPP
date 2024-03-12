35df97c695c336a52d2b00699cb64426445be3a3
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-12 14:14:57 +0200

    3rd commit( create app 'food')

--- food/views.py
+++ food/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Hello, world! This is my site.")
