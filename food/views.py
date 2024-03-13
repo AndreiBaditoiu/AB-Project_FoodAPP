be81bb5a7fccbf656fe05b29edf8f88e3b6b94b8
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-13 19:44:20 +0200

    Added items in food & modified views

--- food/views.py
+++ food/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h1>This is the food app homepage.</h1>")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 
 def item(request):
