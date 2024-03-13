182f110c216b2e0e6667848d30b88f228ceaa6c5
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-13 23:21:06 +0200

    Added detail view, new path to item by id

--- food/urls.py
+++ food/urls.py
@@ -2,8 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # /food/
     path('', views.index, name='index'),
+    # /food/1 (id 1)--exemplu
+    path('<int:item_id>/', views.detail, name='detail'),
     path('item/', views.item, name='item'),
-    path('order/', views.order, name='order'),
-    path('cart/', views.cart, name='cart'),
 ]
--- food/views.py
+++ food/views.py
@@ -31,9 +31,5 @@ def item(request):
     return HttpResponse("<h1>This is an item view</h1>")
 
 
-def order(request):
-    return HttpResponse("<h1>This is an order page</h1>")
-
-
-def cart(request):
-    return HttpResponse("<h1>This is your cart</h1>")
+def detail(request, item_id):
+    return HttpResponse("This is item no/id: %s" % item_id)
