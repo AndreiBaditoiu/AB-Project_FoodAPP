0c933a64a77198b6b0428d872c7f46d01cee7a22
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-18 18:32:21 +0200

    Added static/Images/convertor, added logo. modified base, views.

---
+++ food/static/Images/convertor.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+img=Image.open("Logo.webp")
+
+img.save("Logo.jpg", "JPEG")
+
+
--- food/views.py
+++ food/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
-from django.urls import reverse
+
 from django.template import loader
 
 
