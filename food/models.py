0ad1c7cf4a013df39050e7801a0622bb809d78c3
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-25 18:10:50 +0200

    Modified models, index html, updated db

--- food/models.py
+++ food/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -7,9 +8,9 @@ class Item(models.Model):
 
     def __str__(self):
         return self.item_name
+
     # str repr-(afisarea produsului-nume, decriere etc nu dupa id!
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-
-
+    item_image = models.CharField(max_length=500, default ="https://cdn.dribbble.com/users/1012566/screenshots/4187820/media/985748436085f06bb2bd63686ff491a5.jpg?resize=400x300&vertical=center")
