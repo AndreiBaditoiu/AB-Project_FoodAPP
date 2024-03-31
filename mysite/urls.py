880131500dd571d2dc4beee8816e780608ee69aa
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-31 16:45:20 +0300

    Modified settings.py (login redirect), main urls, created login template

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,16 @@ from django.contrib import admin
 from django.urls import include, path
 from food import views
 from users import views as user_views
+from django.contrib.auth import views as authentication_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('', views.index, name='index'),
-    path('register/', user_views.register, name='register')
+    path('register/', user_views.register, name='register'),
+    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
 
 ]
