bfdae0d46407ce716ccfc750e22acb55f8e6bc71
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-03-31 18:22:13 +0300

    modified views(app), added logout view, addeed logout template.

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,13 +21,16 @@ from users import views as user_views
 from django.contrib.auth import views as authentication_views
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
     path('', views.index, name='index'),
     path('register/', user_views.register, name='register'),
     path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', user_views.logout_view, name='logout'),
+
 
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import RegisterForm
+from django.contrib.auth import logout
 
 
 # Create your views here.
@@ -13,9 +14,16 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, your account has been created')
-            return redirect('food:index')
+            return redirect('login')
 
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
 # 'form.is_valid()' verifica username ( su nu fie duplicat).
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
