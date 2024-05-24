3b96fd47d0631a197750eb865c2c5bfd8f860fcc
AndreiBaditoiu <andrei.baditoiu@gmail.com>
2024-05-24 11:53:32 +0300

    Modified Admin Panel.

--- food/admin.py
+++ food/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
+
 from .models import Item
 from .models import Order
 
 # Register your models here.
 
+admin.site.site_header = 'FoodApp Admin'
+admin.site.site_title = 'FoodApp Admin'
+admin.site.index_title = 'Welcome to FoodApp'
 admin.site.register(Item)
 admin.site.register(Order)
